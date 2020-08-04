f42600e33a4dd831f073875eab4c6a9cc25973ef
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-08-04 18:11:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function greet(timeString){
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(msg){
- document.querySelector('#greeting').innerHTML=msg; 
+ document.querySelector('#greeting').innerHTML='TEST'; 
 }
